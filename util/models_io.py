fe7ee6b2e0fbf6631e75aa8f33c52ab3d58090cb
SantoshMore2020 <71905967+SantoshMore2020@users.noreply.github.com>
2025-10-12 15:33:47 -0700

    Update and rename file_operations.py to models_io.py
    Renamed file_operations.py to models_io.py. Made only two save and load functions, one for RL and the other for all other models.

--- util/models_io.py
+++ util/models_io.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import os, json
 
-def save_models(save_dir, actor, critic, vae_encoder,
+def save_RLmodels(save_dir, actor, critic, vae_encoder,
                     actor_opt, critic_opt, vae_opt,
                     bocpd_cfg, meta, step=None):
     os.makedirs(save_dir, exist_ok=True)
@@ -19,7 +19,7 @@ def save_models(save_dir, actor, critic, vae_encoder,
     print(f"Saved all models + optimizers at step {step}")
 
 
-def load_models(load_dir, actor, critic, vae_encoder,
+def load_RLmodels(load_dir, actor, critic, vae_encoder,
                     actor_opt, critic_opt, vae_opt,
                     device, step=None):
     tag = f"_step{step}" if step is not None else ""
@@ -35,28 +35,28 @@ def load_models(load_dir, actor, critic, vae_encoder,
     print(f"Loaded models/opts at step {step}")
     return bocpd_cfg, meta
 
-def save_lstm_models(save_dir, lstm, vae_encoder,
-                    lstm_opt, vae_opt,
+def save_models(save_dir, model, vae_encoder,
+                    model_opt, vae_opt,
                     bocpd_cfg, meta, step=None):
     os.makedirs(save_dir, exist_ok=True)
     tag = f"_step{step}" if step is not None else ""
 
-    torch.save(lstm.state_dict(), os.path.join(save_dir, f"lstm{tag}.pth"))
+    torch.save(model.state_dict(), os.path.join(save_dir, f"model{tag}.pth"))
     torch.save(vae_encoder.state_dict(), os.path.join(save_dir, f"vae{tag}.pth"))
-    torch.save(lstm_opt.state_dict(), os.path.join(save_dir, f"lstm_opt{tag}.pth"))
+    torch.save(model_opt.state_dict(), os.path.join(save_dir, f"model_opt{tag}.pth"))
     torch.save(vae_opt.state_dict(), os.path.join(save_dir, f"vae_opt{tag}.pth"))
     with open(os.path.join(save_dir, f"bocpd_cfg{tag}.json"), "w") as f: json.dump(bocpd_cfg, f)
     with open(os.path.join(save_dir, f"meta{tag}.json"), "w") as f: json.dump(meta, f)
     print(f"Saved all models + optimizers at step {step}")
 
 
-def load_lstm_models(load_dir, lstm, vae_encoder,
-                    lstm_opt, vae_opt,
+def load_models(load_dir, model, vae_encoder,
+                    model_opt, vae_opt,
                     device, step=None):
     tag = f"_step{step}" if step is not None else ""
-    lstm.load_state_dict(torch.load(os.path.join(load_dir, f"lstm{tag}.pth"), map_location=device))
+    model.load_state_dict(torch.load(os.path.join(load_dir, f"model{tag}.pth"), map_location=device))
     vae_encoder.load_state_dict(torch.load(os.path.join(load_dir, f"vae{tag}.pth"), map_location=device))
-    lstm_opt.load_state_dict(torch.load(os.path.join(load_dir, f"lstm_opt{tag}.pth"), map_location=device))
+    model_opt.load_state_dict(torch.load(os.path.join(load_dir, f"model_opt{tag}.pth"), map_location=device))
     vae_opt.load_state_dict(torch.load(os.path.join(load_dir, f"vae_opt{tag}.pth"), map_location=device))
 
     with open(os.path.join(load_dir, f"bocpd_cfg{tag}.json")) as f: bocpd_cfg = json.load(f)
