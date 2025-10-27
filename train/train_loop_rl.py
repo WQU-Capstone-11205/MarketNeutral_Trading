35747d821004014f1016a55e2101a1318e18acb6
SantoshMore2020 <71905967+SantoshMore2020@users.noreply.github.com>
2025-10-26 18:43:34 -0700

    Update train_loop_rl.py
    Updated the whole training loop for RL, some of the changes are: 1) The BOCPD update function is updated so that the shift appears here, 2) It always starts from the first step now, instead of ignoring the samples of size "state_window", which is removed now. 3) Adding the best_params as parameters, so when hyperparameters are tuned, they can be directly passed to this function.

--- train/train_loop_rl.py
+++ train/train_loop_rl.py
@@ -18,20 +18,21 @@ from ml_dl_models.rnn_vae import VAEEncoder, vae_loss
 from ml_dl_models.actor_critic import Actor
 from ml_dl_models.actor_critic import Critic
 from util.weighted_replay_buffer import WeightedReplayBuffer
+from util.eval_strategy import evaluate_strategy
 from util.models_io import save_RLmodels
 
-def train_loop(
+def train_rl_loop(
     stream,
-    bocpd_params: Dict[str, List[Any]]=None,
-    vae_params: Dict[str, List[Any]]=None,
-    rl_params: Dict[str, List[Any]]=None,
-    joint_params: Dict[str, List[Any]]=None,
-    num_epochs=50,
+    bocpd_params=None,
+    vae_params=None,
+    rl_params=None,
+    joint_params=None,
+    num_epochs=25,
     save_dir="checkpoints",
     total_steps=10000,
     device='cpu'
 ):
-    state_window=tuning_params['state_window']
+    state_window=joint_params['state_window']
     seq_len_for_vae=vae_params['vae_seq_len']
     bocpd_hazard=bocpd_params['hazard']
     if isinstance(stream, pd.Series):
@@ -40,66 +41,71 @@ def train_loop(
     else:
         data = np.asarray(stream)
         dates = None
-    
+
     bocpd = BOCPD(
-                  ConstantHazard(bocpd_hazard), 
+                  ConstantHazard(bocpd_hazard),
                   StudentT(
-                            mu=bocpd_params['mu'], 
-                            kappa=bocpd_params['kappa'], 
-                            alpha=bocpd_params['alpha'], 
+                            mu=bocpd_params['mu'],
+                            kappa=bocpd_params['kappa'],
+                            alpha=bocpd_params['alpha'],
                             beta=bocpd_params['beta']
                     )
             )
-  
-    #input_dim = 2  # [return, bocpd_prob] per timestep into encoder
-    #z_dim = 16
+
     state_dim = state_window  # using flattened returns as state; in practice use richer features
-                
-    actor = Actor(state_dim=state_window, z_dim=rl_params['state_dim'], hidden_dim=rl_params['hidden_dim'], action_dim=rl_params['action_dim']).to(device)
-    critic = Critic(state_dim=state_window, z_dim=rl_params['state_dim'], hidden_dim=rl_params['hidden_dim']).to(device)
-    encoder = VAEEncoder(input_dim=vae_params['input_dim'], hidden_dim=vae_params['hidden_dim'], z_dim=vae_params['latent_dim'], seq_len=seq_len_for_vae).to(device)
-    
+
+    actor = Actor(
+                state_dim=state_window, 
+                z_dim=rl_params['state_dim'], 
+                hidden_dim=rl_params['hidden_dim'], 
+                action_dim=rl_params['action_dim']
+                ).to(device)
+
+    critic = Critic(
+                state_dim=state_window, 
+                z_dim=rl_params['state_dim'], 
+                hidden_dim=rl_params['hidden_dim']
+                ).to(device)
+
+    encoder = VAEEncoder(
+                input_dim=vae_params['input_dim'], 
+                hidden_dim=vae_params['hidden_dim'], 
+                z_dim=vae_params['latent_dim'], 
+                seq_len=seq_len_for_vae
+                ).to(device)
+
     actor_opt = optim.Adam(actor.parameters(), lr=rl_params['lr'])
     critic_opt = optim.Adam(critic.parameters(), lr=rl_params['lr'])
     opt_vae = optim.Adam(encoder.parameters(), lr=vae_params['lr'])
     buffer = WeightedReplayBuffer(capacity=30000)
+    best_val_sharpe = -np.inf
 
-    # --- training loop
+    # training loop
     for epoch in range(num_epochs):
         rms = RunningMeanStd()
         # action noise base sigma
         base_action_sigma = joint_params['base_action_sigma']
         # walkthrough
-        T = min(total_steps, len(data) - state_window - 1)
-        rms.update(data[:state_window])
+        T = min(total_steps, len(data) - 1)
 
         # initialize state: last `state_window` returns
-        idx = 0
-        state_returns = list(data[idx: idx + state_window])
-        idx += state_window
+        state_returns = [0.0]*(state_window-1)
+        state_returns.append(data[0])
         last_action = 0.0
-        cp_list = [0.0]*state_window
-        rt_mle = [0]*state_window
-        cp_flag_list = [0]*state_window
-        out_recon = [0]*state_window
-        actions_pnl = [0]*state_window
-
-        total_recon, total_kl = 0, 0
+        out_recon = []
+        actions_pnl = []
+        total_recon, total_kl, total_policy_loss = 0, 0, 0
 
         for step in trange(T):
-            cur_ret = data[idx]
+            cur_ret = data[step]
             rms.update([cur_ret])
             # BOCPD expects scalar observation -> use normalized return
             norm_ret = float((cur_ret - rms.mean) / (math.sqrt(rms.var) + 1e-8))
-            change_prob = bocpd.update(norm_ret)  # float in [0,1]
-            cp_list.append(change_prob)
-            rt_mle.append(bocpd.rt)
-            cp_flag = 1 if rt_mle[idx] < rt_mle[idx-1] else 0
-            cp_flag_list.append(cp_flag)
+            change_prob, cp_flag = bocpd.update(norm_ret)  # float in [0,1]
 
             # build encoder input sequence (seq_len_for_vae)
-            seq_start = max(0, idx - seq_len_for_vae + 1)
-            seq_rets = data[seq_start: idx + 1]
+            seq_start = max(0, step - seq_len_for_vae + 1)
+            seq_rets = data[seq_start: step + 1]
             # pad if needed
             if len(seq_rets) < seq_len_for_vae:
                 pad = np.zeros(seq_len_for_vae - len(seq_rets))
@@ -110,8 +116,7 @@ def train_loop(
             seq_inp_t = torch.tensor(seq_inp, dtype=torch.float32).to(device)
             # # --- VAE encoder ---
             x_hat, mu, logvar, z_t = encoder(seq_inp_t)
-            kl_w_per_epoch = min(1.0, epoch / 100)
-            loss_vae, recon_loss, kl_loss = vae_loss(seq_inp_t, x_hat, mu, logvar, kl_weight=kl_w_per_epoch)
+            loss_vae, recon_loss, kl_loss = vae_loss(seq_inp_t, x_hat, mu, logvar, kl_weight=vae_params['kl_wt'])
             opt_vae.zero_grad(); loss_vae.backward(); opt_vae.step()
 
             # keep denormalized reconstruction for plotting if desired
@@ -138,9 +143,9 @@ def train_loop(
             actions_pnl.append(action)
             # interpret action: e.g., fraction of capital to long (positive) or short (negative)
             # reward: simple PnL = action * next_return
-            next_ret = data[idx + 1]
+            next_ret = data[step + 1]
             reward = float(action * next_ret)
-            
+
             # store transition in buffer with initial weight 1.0
             buffer.push(
                 state_norm.astype(np.float32),
@@ -154,7 +159,7 @@ def train_loop(
             # if change_prob large, upweight recent transitions
             if cp_flag == 1:
                 buffer.upweight_recent(window=200, multiplier=joint_params['wt_multplier'])
-            
+
             # periodic updates
             if buffer.size() >= joint_params['buffer_size_updates'] and step % 8 == 0:
                 batch = buffer.sample(joint_params['sample_batch_size'])
@@ -165,7 +170,7 @@ def train_loop(
                 rewards = torch.tensor(np.stack([b.reward for b in batch]), dtype=torch.float32).unsqueeze(-1).to(device)
 
                 # critic update
-                values = critic(states, torch.zeros(states.size(0), z_dim).to(device))  # critic input placeholder z
+                values = critic(states, torch.zeros(states.size(0), rl_params['state_dim']).to(device))  # critic input placeholder z
                 targets = rewards
                 critic_loss = nn.MSELoss()(values, targets)
                 critic_opt.zero_grad(); critic_loss.backward(); critic_opt.step()
@@ -173,28 +178,41 @@ def train_loop(
                 # actor update
                 with torch.no_grad():
                     adv = (rewards - values).detach()
-                pred_actions = actor(states, torch.zeros(states.size(0), z_dim).to(device))
+                pred_actions = actor(states, torch.zeros(states.size(0), rl_params['state_dim']).to(device))
                 actor_loss = - (pred_actions * adv).mean()
                 actor_opt.zero_grad(); actor_loss.backward(); actor_opt.step()
-                
+                total_policy_loss += actor_loss.item()
+
+            # update state
+            state_returns.append(next_ret)
+
             total_recon += recon_loss
             total_kl += kl_loss
 
-            # slide window
-            state_returns.append(next_ret)
-            idx += 1
-
         if (epoch + 1) == num_epochs:
             print()
-        print(f"Epoch {epoch:03d} | recon loss = {(total_recon/len(data)):.4f} | kl loss = {(total_kl/len(data)):.4f}")
+        avg_recon = total_recon / len(data)
+        avg_kl = total_kl / len(data)
+        avg_policy = total_policy_loss / len(data)
+        print(f"Epoch {epoch:03d} | recon loss = {avg_recon:.4f} | kl loss = {avg_kl:.4f} | policy loss = {avg_policy:.4f}")
 
-        # Save every 10 epochs
-        if (epoch + 1) % 10 == 0 or (epoch + 1) == num_epochs:
-            meta = {"epoch": epoch, "recon loss": (total_recon/len(data)), "kl loss": (total_kl/len(data))}
+        # ============================================================
+        #   Save models for best Sharpe ratio
+        # ============================================================
+        val_metrics = evaluate_strategy(actions_pnl)
+        val_sharpe = val_metrics["sharpe_ratio"]
+
+        print(f"Val Sharpe={val_sharpe:.3f}")
+
+        # --- save best checkpoint ---
+        if val_sharpe > best_val_sharpe:
+            best_val_sharpe = val_sharpe
+            meta = {"epoch": epoch, "recon loss": (avg_recon), "kl loss": (avg_kl), "policy loss" : (avg_policy)}
             bocpd_cfg = {"bocpd_hazard": bocpd_hazard}
             save_RLmodels(save_dir, actor, critic, encoder,
                             actor_opt, critic_opt, opt_vae,
                             bocpd_cfg, meta)
+            print(f"Saved best models at epoch {epoch:03d} (Sharpe={val_sharpe:.3f})")
 
     np.savez(os.path.join(save_dir, "rms_stats.npz"), mean=rms.mean, var=rms.var)
     print("Training complete.")
