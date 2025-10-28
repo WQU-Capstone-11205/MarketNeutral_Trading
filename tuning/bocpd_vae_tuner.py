7f1d1b8650496ea8b6139de57649f13396976d8a
SantoshMore2020 <71905967+SantoshMore2020@users.noreply.github.com>
2025-10-28 11:09:02 -0700

    Update bocpd_vae_tuner.py
    (for debugging only)  Added risk-adjusted returns as auxiliary input to VAE

--- tuning/bocpd_vae_tuner.py
+++ tuning/bocpd_vae_tuner.py
@@ -134,7 +134,7 @@ class BOCPD_VAE_Tuner:
                     cur_ret = data.iloc[i] - data.iloc[i-1]
                 state_returns = np.append(state_returns, cur_ret)
                 seq_diff = state_returns[-seq_len_vae:]
-                #seq_diff = seq_diff / (math.sqrt(rms.var) + 1e-8)
+                seq_diff = seq_diff / (math.sqrt(rms.var) + 1e-8)
                 # pad if needed
                 if len(seq_rets) < seq_len_vae:
                     pad = np.zeros(seq_len_vae - len(seq_rets))
