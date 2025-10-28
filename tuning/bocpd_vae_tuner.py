f66659b3755302ed41636741a85f25695870a983
SantoshMore2020 <71905967+SantoshMore2020@users.noreply.github.com>
2025-10-28 13:54:16 -0700

    Update bocpd_vae_tuner.py
    Updated the best parameters for the BOCPD model and reverted the changes for risk-adjusted returns.

--- tuning/bocpd_vae_tuner.py
+++ tuning/bocpd_vae_tuner.py
@@ -48,9 +48,9 @@ class BOCPD_VAE_Tuner:
     best_bocpd_params = {
         "hazard": 50,
         "mu": 0,
-        "kappa": 10.0,
-        "alpha": 10.0,
-        "beta": 10.0
+        "kappa": 0.1,
+        "alpha": 1.0,
+        "beta": 0.1
     }
 
     best_vae_params = {
@@ -134,7 +134,7 @@ class BOCPD_VAE_Tuner:
                     cur_ret = data.iloc[i] - data.iloc[i-1]
                 state_returns = np.append(state_returns, cur_ret)
                 seq_diff = state_returns[-seq_len_vae:]
-                seq_diff = seq_diff / (math.sqrt(rms.var) + 1e-8)
+                # seq_diff = seq_diff / (math.sqrt(rms.var) + 1e-8)
                 # pad if needed
                 if len(seq_rets) < seq_len_vae:
                     pad = np.zeros(seq_len_vae - len(seq_rets))
