be952a1b688371976e83e30c2a5fe2001fe26697
SantoshMore2020 <71905967+SantoshMore2020@users.noreply.github.com>
2025-11-10 14:32:41 -0800

    Update distance_spread.py
    Creating a cointegration spread. Currently, distance and cointegration spreads are identical. The reason is that we want to rename the distance spread to cointegration spread, but in order to avoid backward compatibility for the previous, we will keep it this way for a few days and then remove distance spread or change distance spread code to match its functionality if required.

--- data_loading/distance_spread.py
+++ data_loading/distance_spread.py
@@ -15,3 +15,16 @@ def distance_spread(tickers, start_date=None, end_date=None):
     spread = spread.dropna()
     return spread;
 
+def cointegration_spread(tickers, start_date=None, end_date=None):
+    if end_date is None:
+        end_date = dt.date.today()
+    if start_date is None:
+        start_date = end_date - dt.timedelta(days=365*2)  # ~2 years
+    data = yf.download(tickers, start=start_date, end=end_date, auto_adjust=False, multi_level_index=False)['Adj Close']
+    first = tickers[0]
+    second = tickers[1]
+    hedge_ratio = data[first].mean() / data[second].mean()
+    spread = data[first] - hedge_ratio * data[second]
+    spread = spread.dropna()
+    return spread;
+
