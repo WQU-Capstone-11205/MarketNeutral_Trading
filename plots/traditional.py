9d02114218d864da9cb5bd9ff8d08178858d636f
SantoshMore2020 <71905967+SantoshMore2020@users.noreply.github.com>
2025-11-10 17:41:08 -0800

    Create traditional.py
    Adding maxdrawdown and trend plots for comparison for Traditional models

---
+++ plots/traditional.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def compare_trends_plot(input_spread, results):
+    fig, ax1 = plt.subplots(figsize=(12, 5))
+
+    ax1.plot(input_spread.index, input_spread, label='Spread', color='gray')
+    ax1.set_ylabel('USD/pair trading', color='gray')
+    ax1.tick_params(axis='y', labelcolor='gray')
+
+    ax2 = ax1.twinx()
+    ax2.plot(input_spread.index[:-1], results, label='Profit & Loss', color='blue')
+    ax2.set_ylabel('USD/pair trading', color='blue')
+    ax2.tick_params(axis='y', labelcolor='blue')
+    ax1.set_xlabel('Date')
+    plt.title('Profit & Loss (results) vs Spread (input)')
+    fig.legend(loc="upper right", bbox_to_anchor=(1,1), bbox_transform=ax1.transAxes)
+    plt.show()
+
+def drawdown_plot(returns):
+    cum_pnl = np.cumsum(returns)
+    rolling_max = np.maximum.accumulate(cum_pnl)
+    drawdown = (rolling_max - cum_pnl) # / (rolling_max + 1e-8)
+    plt.plot(returns.index, drawdown, label="Profit & Loss", color="blue")
+    plt.xlabel("Date")
+    plt.ylabel("Drawdown")
+    plt.legend()
+    plt.title("Portfolio returns drawdown (Traditional)")
+    plt.show()
