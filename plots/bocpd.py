634b044c7533f866bc12b66afc0a361ce87deb0e
SantoshMore2020 <71905967+SantoshMore2020@users.noreply.github.com>
2025-11-16 18:28:43 -0800

    Update bocpd.py
    Adding vae, drawdown, and compare plots to the BOCPD module

--- plots/bocpd.py
+++ plots/bocpd.py
@@ -32,14 +32,13 @@ def plot_change_points(spread):
 
 def plot_rt_change_probs(data, rt_mle, change_probs):
     sz = len(data)
-    
     # --- Plot results ---
     fig, ax1 = plt.subplots(figsize=(12, 5))
     
     ax1.plot(data.index[:sz-1], data[:sz-1], label='Original Spread', color='gray')
     ax1.set_ylabel('USD/pair trading', color='gray')
     ax1.tick_params(axis='y', labelcolor='gray')
-    
+
     ax2 = ax1.twinx()
     ax2.plot(data.index[:sz-1],change_probs, label='Change Probability', color='red')
     ax2.set_ylabel('Change Probability', color='red')
@@ -51,7 +50,42 @@ def plot_rt_change_probs(data, rt_mle, change_probs):
     ax3.plot(data.index[:sz-1],rt_mle, label='Most Likely Run Length', color='blue', linestyle='--')
     ax3.set_ylabel('Most Likely Run Length', color='blue')
     ax3.tick_params(axis='y', labelcolor='blue')
+    
     ax1.set_xlabel('Date')
     plt.title('BOCPD: Change Probability Over Time (for Test spread)')
     fig.legend(loc="upper right", bbox_to_anchor=(1,1), bbox_transform=ax1.transAxes)
+    plt.show()
+
+def vae_plot(input_spread, recon_spread):
+    plt.plot(input_spread.index, input_spread, label="Original Spread")
+    plt.plot(input_spread.index, recon_spread, label="Reconstructed Spread")
+    plt.xlabel("Date")
+    plt.ylabel("USD/pair trading")
+    plt.legend();
+    plt.title("VAE: Original vs Reconstructed Spread")
+    plt.show()
+  
+def compare_trends_plot(input_spread, results):
+    fig, ax1 = plt.subplots(figsize=(12, 5))
+    ax1.plot(input_spread.index, input_spread, label='Distance Spread', color='gray')
+    ax1.set_ylabel('USD/pair trading', color='gray')
+    ax1.tick_params(axis='y', labelcolor='gray')
+    ax2 = ax1.twinx()
+    ax2.plot(input_spread.index[:-1], results, label='Profit & Loss', color='blue')
+    ax2.set_ylabel('USD/pair trading', color='blue')
+    ax2.tick_params(axis='y', labelcolor='blue')
+    ax1.set_xlabel('Date')
+    plt.title('Profit & Loss (results) vs Distance Spread (input)')
+    fig.legend(loc="upper right", bbox_to_anchor=(1,1), bbox_transform=ax1.transAxes)
+    plt.show()
+
+def drawdown_plot(returns):
+    cum_pnl = np.cumsum(returns)
+    rolling_max = np.maximum.accumulate(cum_pnl)
+    drawdown = (rolling_max - cum_pnl) # / (rolling_max + 1e-8)
+    plt.plot(returns.index, drawdown, label="Profit & Loss", color="blue")
+    plt.xlabel("Date")
+    plt.ylabel("Drawdown")
+    plt.legend()
+    plt.title("Portfolio returns drawdown (BOCPD+VAE+RL)")
     plt.show()
